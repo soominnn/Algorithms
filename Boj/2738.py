219566263334103cb7839047341a492f8183a3be
soominnn <qkrtnals970804@gmail.com>
2022-11-29 02:57:26 +0900

    행렬 덧셈

---
+++ Boj/2738.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+A = []
+B = []
+for i in range(N):
+    row = list(map(int, input().split()))
+    A.append(row)
+
+for i in range(N):
+    row = list(map(int, input().split()))
+    B.append(row)
+
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end = ' ')
+    print()
+
+    
