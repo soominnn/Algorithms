3d92f1ba07107a2f67a7cc1a17b126a7f5126d96
soomin <qkrtnals970804@gmail.com>
2023-01-16 00:22:11 +0900

    level 2

--- Programmers/괄호 회전하기.py
+++ Programmers/괄호 회전하기.py
@@ -1,24 +1,24 @@
+from collections import deque
+
 def solution(s):
     answer = 0
-    tmp = list(s)
-    
-    for _ in range(len(s)):
- 
+    s = deque(s)
+    for i in range(len(s)):
         stack = []
-        for i in range(len(tmp)):
-            if len(stack) > 0:
-                if stack[-1] == '[' and tmp[i] == ']':
+        for ss in s:
+            if stack:
+                if ss == ')' and stack[-1] == '(':
                     stack.pop()
-                elif stack[-1] == '(' and tmp[i] == ')':
+                elif ss == '}' and stack[-1] == '{':
                     stack.pop()
-                elif stack[-1] == '{' and tmp[i] == '}':
+                elif ss == ']' and stack[-1] == '[':
                     stack.pop()
                 else:
-                    stack.append(tmp[i])
+                    stack.append(ss)
             else:
-                stack.append(tmp[i])
-        if len(stack) == 0:
-            answer += 1
-        tmp.append(tmp.pop(0))
- 
+                stack.append(ss)
+        if not stack:
+            answer += 1 
+        s.rotate(-1)
+    
     return answer
