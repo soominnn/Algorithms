a6e040fa96323eeb77d8369371d3fce181be854d
soomin <qkrtnals970804@gmail.com>
2023-10-18 20:43:58 +0900

    level 2

---
+++ Programmers/미로 탈출.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+def bfs(start, end, maps):
+    # 탐색할 방향
+    dx = [1, 0, 0, -1]
+    dy = [0, 1, -1, 0]
+    
+    n = len(maps)
+    m = len(maps[0])
+    visited = [[False] * m for _ in range(n)]
+    queue = deque()
+    flag = False
+    
+    # 초깃값 설정
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] == start:
+                queue.append((i, j, 0))
+                visited[i][j] = True
+                flag = True;
+                break
+        if flag:
+            break
+    
+    while queue:
+        y, x, cost = queue.popleft()
+        if maps[y][x] == end:
+            return cost
+        
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            
+            if 0 <= ny < n and 0 <= nx < m and maps[ny][nx] != 'X':
+                if not visited[ny][nx]:
+                    queue.append((ny, nx, cost + 1))
+                    visited[ny][nx] = True
+    
+    return -1 
+
+def solution(maps):
+    path1 = bfs('S', 'L', maps)
+    path2 = bfs('L', 'E', maps)
+    
+    if path1 != -1 and path2 != -1:
+        return path1 + path2
+    
+    return -1
