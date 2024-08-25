7cc979814b1be04a844a4722cd1e8fab831886c3
soomin <qkrtnals970804@gmail.com>
2024-08-25 23:10:11 +0900

    괄호

---
+++ Boj/9012.py
@@ -0,0 +1,13 @@
+n = int(input())
+for _ in range(n):
+    arr = input()
+    stack = []
+    for a in arr:
+        if a == ")" and len(stack) and stack[-1] == "(":
+            stack.pop()
+        else:
+            stack.append(a)
+    if stack:
+        print("no")
+    else:
+        print("yes")
