0263b5bb878dfb10b3c938906ec66ce77e2c14ca
soomin <qkrtnals970804@gmail.com>
2024-09-01 00:54:35 +0900

    요세푸스 문제 0

---
+++ Boj/11866.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+
+arr = list(range(1, n + 1))
+start_idx = 0
+res = []
+
+for i in range(n):
+    start_idx = (start_idx + k - 1) % len(arr)
+    res.append(arr.pop(start_idx))
+print(res)
+print("<" + str(res)[1:-1] + ">")
